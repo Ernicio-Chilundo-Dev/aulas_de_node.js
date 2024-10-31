83709b3087ae1db6d3b3dc992f71227f115ca429
Ernicio-Chilundo-Dev <erniciojermiaschilund@gmail.com>
2024-10-31 16:56:38 +0200

    aula03b feita com sucesso

--- aula3/aula03b.js
+++ aula3/aula03b.js
@@ -6,6 +6,9 @@ const host = '127.0.0.1';
 const servidor = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.write(req.url)
+    const p = url.parse(req.url, true).query
+    res.write('<br>'+p.nome)
+    res.write('<br>'+p.curso)
     res.end()
 })
 
