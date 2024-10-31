13246c93dfd678f59474f2ff661dd0bc38fc9734
Ernicio-Chilundo-Dev <erniciojermiaschilund@gmail.com>
2024-10-31 15:54:58 +0200

    aula 2 absurvida com sucesso

--- aula2.js
+++ aula2.js
@@ -1,8 +1,8 @@
-const http = require('http');
-http.createServer((requist, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
+const http = require('http')
+http.createServer((requist,response)=>{
+    response.writeHead(200,{
+        'content-Type':'text/plain'
     })
-    response.write("CFBcurso\n Aula de node.js")
+    response.write("Cfbcursos\n curso de node.js")
     response.end()
-}).listen(1337);
+}).listen(1337)
